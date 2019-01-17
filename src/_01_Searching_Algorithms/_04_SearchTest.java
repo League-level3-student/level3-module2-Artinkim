3b12fbb2de4b2c9d992991b85f37cc32513e2ff0
Artin <artin513@sbcglobal.net>
2019-01-17 10:24:45 -0800

    Done Linear And BinarySearch

--- src/_01_Searching_Algorithms/_04_SearchTest.java
+++ src/_01_Searching_Algorithms/_04_SearchTest.java
@@ -4,6 +4,8 @@ import static org.junit.jupiter.api.Assertions.*;
 
 import org.junit.jupiter.api.Test;
 
+import _00_Sorting_Algorithms._00_SortedArrayChecker;
+
 class _04_SearchTest {
 
 	// A MINIMUM OF 3 TESTS ARE REQUIRED FOR EACH METHOD
@@ -11,12 +13,24 @@ class _04_SearchTest {
 	@Test
 	public void testLinearSearch() {
 		//1. use the assertEquals method to test your linear search method.
+		String[] list = {"carrot", "pineapple", "mango", "pear"};
+		String[] list2 = {"apple", "banana", "carrot", "grape"};
+		
+		assertEquals(_00_LinearSearch.linearSearch(list,"mango"),2);
+		assertEquals(_00_LinearSearch.linearSearch(list2,"banana"),1);
+		assertEquals(_00_LinearSearch.linearSearch(list2,"grape"),3);
 	}
 
 	@Test
 	public void testBinarySearch() {
 		//2. use the assertEquals method to test your binary search method.
 		//   remember that the array must be sorted
+		String[] list = {"apple", "banana", "carrot", "grape"};
+		
+		assertEquals(_00_LinearSearch.linearSearch(list,"banana"),1);
+		assertEquals(_00_LinearSearch.linearSearch(list,"grape"),3);
+		assertEquals(_00_LinearSearch.linearSearch(list,"apple"),0);
+
 	}
 	
 	@Test
