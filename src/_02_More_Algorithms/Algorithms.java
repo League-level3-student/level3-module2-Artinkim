5d389e28d057e8c88267ec9373f06f0ad8bee572
Artin <Artin@DESKTOP-L54PHGT.san.rr.com>
2019-01-19 23:54:50 -0800

    Last algorithms

--- src/_02_More_Algorithms/Algorithms.java
+++ src/_02_More_Algorithms/Algorithms.java
@@ -4,6 +4,38 @@ import java.util.List;
 
 public class Algorithms {
 	public static int findBrokenEgg(List<String> eggs) {
-		return 0;
+		for(int i = 0;i<eggs.size()-1;i++) {
+			if(eggs.get(i).equals("cracked")) {
+				return i;
+			}
+		}
+		return -1;
+	}
+	public static int countPearls(List<Boolean> oy) {
+		int r = 0;
+		for(int i = 0;i<oy.size();i++) { 
+			if(oy.get(i)) {
+				r++;
+			}
+		}
+		return r;
+	}
+	public static double findTallest(List<Double> peeps) {
+		double tallestPerson = 0.0;
+			for(int i = 0;i<peeps.size();i++) {
+				if(tallestPerson<peeps.get(i)) {
+					tallestPerson = peeps.get(i);
+				}
+			}
+		return tallestPerson;
+	}
+	public static String findLongestWord(List<String> words) {
+		String longest = "";
+		for(int i = 0;i<words.size();i++) {
+			if(longest.length()<words.get(i).length()) {
+				longest = words.get(i);
+			}
+		}
+		return longest;
 	}
 }
